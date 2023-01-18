5d193f0f87f68629d5d0265208834b15e519d560
Andrew Wentzel <tehwentzel@gmail.com>
2023-01-18 16:49:27 -0600

    setup stuff

---
+++ python/Constants.py
@@ -0,0 +1,58 @@
+class Const():
+
+    #for troubleshooting, will migrate to own file
+    data_dir = "../data/" #private data
+    resource_dir = "../resources/" #public data (can be on github)
+    
+    dicom_dir = data_dir + '/DICOM/'
+    dicom_test_dir = data_dir + '/SMART/'
+    
+    organ_list = [
+        'Esophagus',#
+         'Spinal_Cord',#
+         'Lt_Brachial_Plexus',
+         'Rt_Brachial_Plexus',
+         'Cricopharyngeal_Muscle',#
+#          'Lt_thyroid_lobe',
+#          'Rt_thyroid_lobe',
+         'Cricoid_cartilage',#
+         'IPC',#
+         'MPC',#
+         'Brainstem',#
+         'Larynx',#
+         'Thyroid_cartilage',#
+         'Rt_Sternocleidomastoid_M',
+         'Rt_Mastoid',
+         'Rt_Parotid_Gland',
+         'Rt_Medial_Pterygoid_M',
+         'Rt_Lateral_Pterygoid_M',
+         'Rt_Masseter_M',
+         'Lt_Sternocleidomastoid_M',
+         'Lt_Mastoid',
+         'Lt_Parotid_Gland',
+         'Lt_Submandibular_Gland',
+         'Lt_Medial_Pterygoid_M',
+         'Lt_Lateral_Pterygoid_M',
+         'Lt_Masseter_M',
+         'Supraglottic_Larynx',#
+         'SPC',#
+         'Rt_Submandibular_Gland',
+         'Hyoid_bone',#
+         'Soft_Palate',#
+         'Genioglossus_M',#
+         'Tongue',#
+         'Rt_Ant_Digastric_M',
+         'Lt_Ant_Digastric_M',
+         'Mylogeniohyoid_M',#
+         'Extended_Oral_Cavity',#
+         'Mandible',#
+         'Hard_Palate',
+#          'Lt_Posterior_Seg_Eyeball',
+#          'Rt_Posterior_Seg_Eyeball',
+#          'Lt_Anterior_Seg_Eyeball',
+#          'Rt_Anterior_Seg_Eyeball',
+         'Lower_Lip',
+         'Upper_Lip',
+         'Glottic_Area',
+                 ]
+    
